3f1e76693948c309e5cba4c1531d4e6102defbe8
zommymommy <ani.p.wilhelm@gmail.com>
2022-12-06 20:16:59 -0600

    i just made a little teleop i wanna test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/anisubsets/drive.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.anisubsets;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+    // taken from ftc library thing adam sent ->> used to code mecanum but need to import meanings
+   /* MecanumDrive mecanum = new MecanumDrive(frontLeft, frontRight,
+            backLeft, backRight); */
+@TeleOp
+public class drive extends OpMode {
+
+    MotorEx fL, fR, bL, bR;
+    private MecanumDrive drive;
+    private GamepadEx driverOp;
+
+    @Override
+    public void init() {
+        /* instantiate motors */
+
+        drive = new MecanumDrive(fL, fR, bL, bR);
+        driverOp = new GamepadEx(gamepad1);
+    }
+
+    @Override
+    public void loop() {
+        drive.driveRobotCentric(
+                driverOp.getLeftX(),
+                driverOp.getLeftY(),
+                driverOp.getRightY()
+        );
+    }
+
+}
